3dcac9212d58d5874258eb4d0fc22a958d79bbae
frnsimoes <simoes.grcia@gmail.com>
2024-03-16 21:26:13 -0300

    remove go-vim; add dap

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,38 +1,28 @@
 return {
-	"mfussenegger/nvim-dap",
-	"leoluz/nvim-dap-go",
-	config = function()
-		require("dap-go").setup({
-			  dap_configurations = {
-				{
-					-- Must be "go" or it will be ignored by the plugin
-					type = "go",
-					name = "Attach remote",
-					mode = "remote",
-					request = "attach",
-				},
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			'folke/which-key.nvim',
+			'theHamsta/nvim-dap-virtual-text',
+			'nvim-telescope/telescope-dap.nvim',
 		},
-			delve = {
-				-- the path to the executable dlv which will be used for debugging.
-				-- by default, this is the "dlv" executable on your PATH.
-				path = "dlv",
-				-- time to wait for delve to initialize the debug session.
-				-- default to 20 seconds
-				initialize_timeout_sec = 20,
-				-- a string that defines the port to start delve debugger.
-				-- default to string "${port}" which instructs nvim-dap
-				-- to start the process in a random available port
-				port = "${port}",
-				-- additional args to pass to dlv
-				args = {},
-				-- the build flags that are passed to delve.
-				-- defaults to empty string, but can be used to provide flags
-				-- such as "-tags=unit" to make sure the test suite is
-				-- compiled during debugging, for example.
-				-- passing build flags using args is ineffective, as those are
-				-- ignored by delve in dap mode.
-				build_flags = "",
-			},
-		})
-	end,
+
+		config = function()
+			vim.keymap.set('n', '<leader>dc', '<cmd>lua require"dap".continue()<CR>')
+			vim.keymap.set('n', '<leader>db', '<cmd>lua require"dap".toggle_breakpoint()<CR>')
+			vim.keymap.set('n', '<leader>dr', '<cmd>lua require"dap".repl.toggle()<CR>')
+			vim.keymap.set('n', '<leader>do', '<cmd>lua require"dap".step_over()<CR>')
+			vim.keymap.set('n', '<leader>dcb', '<cmd>lua require"dap".clear_breakpoints()<CR>')
+		end,
+	},
+	{
+	 	"leoluz/nvim-dap-go",
+	 	ft = "go",
+	 	dependencies = "mfussenegger/nvim-dap",
+	 	config = function(_, opts)
+			require("dap-go").setup(opts)
+		end
+	},
 }
+
+
