6ebbe462d2ba51d331411728adbe4c957fb350b9
frnsimoes <simoes.grcia@gmail.com>
2025-01-24 10:28:14 -0300

    update colorscheme

--- nvim/lua/plugins/color.lua
+++ nvim/lua/plugins/color.lua
@@ -2,5 +2,18 @@ return {
 	"tjdevries/colorbuddy.nvim",
 	config = function()
 		vim.cmd.colorscheme("gruvbuddy")
+
+		-- Get colorbuddy components after theme is loaded
+		local colorbuddy = require("colorbuddy")
+		local Color = colorbuddy.Color
+		local Group = colorbuddy.Group
+
+		-- Create new white color
+		Color.new("custom_white", "#fafafa")
+
+		-- Set line numbers to our custom white
+		Group.new("LineNr", colorbuddy.colors.custom_white, nil)
+		Group.new("LineNrAbove", colorbuddy.colors.custom_white, nil)
+		Group.new("LineNrBelow", colorbuddy.colors.custom_white, nil)
 	end
 }
