7d69d0f16c01a7d4bd274f134e8fe2a66a992cb5
frnsimoes <simoes.grcia@gmail.com>
2026-07-20 23:46:57 -0300

    remove inspectpacket.sh

--- inspectpacket.sh
+++
@@ -1,1 +0,0 @@
-curl -w "\nDNS: %{time_namelookup}s\nConnect: %{time_connect}s\nTLS: %{time_appconnect}s\nStart Transfer: %{time_starttransfer}s\nTotal: %{time_total}s\n" -o /dev/null -s https://google.com
